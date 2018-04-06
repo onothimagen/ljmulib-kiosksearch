f7d618246200d70b0a3f862f696e39ec88c92b43
Richard Davis <richard.m.davis@gmail.com>
2018-04-06 10:41:28 +0100

    New version with appley buttons, all functions transferred from previous versions.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
    var site;
    var primoSiteFacet;
-   
-   function initialise(){ 
+
+   function initialise(){
   		document.getElementById("searchurl").value = "https://ljmu-primo.hosted.exlibrisgroup.com/primo-explore/search?vid=44JMU_VU1&amp;sortby=rank&amp;query=any,contains,{search}";
     	site = window.location.search;
     	site = site.substring(1);
@@ -19,26 +19,27 @@
     			primoSiteFacet="&amp;mfacet=library,include,44JMU_MAR_LIB,1";
 		}
      	siteTitle = site.charAt(0).toUpperCase() + site.substr(1).toLowerCase();
-    	document.getElementById("siteonly-sitename").innerHTML = siteTitle;
+    	document.getElementById("siteonly-sitename").innerHTML = siteTitle + " Only";
     	return false;
-  } 
+  }
 
 
-  function goSearch(){ 
-  		var t=encodeURIComponent(jQuery("#searchtext").val()); 
+  function goSearch(){
+  		var t=encodeURIComponent(jQuery("#searchtext").val());
   		var booksonly =
-  			document.getElementById("booksonly").checked; 
+  			document.getElementById("booksonly").checked;
   		var siteonly =
-  			document.getElementById("siteonly").checked; 
-  		if (""==t) return false; 
-  		var e=jQuery("#searchtarget").val(); 
-  		var i=document.easysearch.searchurl.value; 
-  		if ( booksonly ) { 
-  			i = i.concat("&amp;facet=rtype,include,books"); 
-  		} 
-  		if ( siteonly ) { 
-  			i = i.concat(primoSiteFacet.valueOf()); 
-  		} 
-  		i=i.replace(/&amp;/g,"&"),window.open(i.replace("{search}",t)) 
-  		return false; 
+  			document.getElementById("siteonly").checked;
+  		if (""==t) return false;
+  		var e=jQuery("#searchtarget").val();
+  		var i=document.easysearch.searchurl.value;
+  		if ( booksonly ) {
+  			i = i.concat("&amp;facet=rtype,include,books");
+  		}
+  		if ( siteonly ) {
+  			i = i.concat(primoSiteFacet.valueOf());
+  		}
+  		i=i.replace(/&amp;/g,"&"),window.open(i.replace("{search}",t))
+      i=i.replace(/&amp;/g,"&"),window.open(i.replace("{search}",t))
+  		return false;
   	}
